e6f462848fc59061c4502123736babd859aa1f8f
barbosa92 <alexrp_92@hotmail.com>
2023-05-31 12:35:25 +0200

    inline comments & readme file

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_cors import CORS
 from modules.routes import api
 
-app = Flask(__name__)
-# After creating the Flask app, you can make all APIs allow cross-origin access.
-CORS(app)
-app.register_blueprint(api)
-
+app = Flask(__name__)  # Create an instance of the Flask application
+CORS(app)  # Enable Cross-Origin Resource Sharing (CORS) for the app
+app.register_blueprint(api)  # Register the blueprint for the API routes
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)  # Run the Flask app in debug mode
--- modules/routes.py
+++ modules/routes.py
@@ -1,52 +1,51 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, request
 from flask_cors import cross_origin
 from modules.utils import DemandRequest, FFTCalculator, FFTPlot, convert_to_utc
-import json
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import json
 import os
 
+
+# Configure Cloudinary
 cloudinary.config(
     cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
     api_key=os.getenv('CLOUDINARY_API_KEY'),
     api_secret=os.getenv('CLOUDINARY_API_SECRET')
 )
 
+# Create a blueprint for API routes
 api = Blueprint("api", __name__)
 
 
-@api.route("/")
-def index():
-    return render_template("index.html")
-
-
-@api.route("/demand", methods=["GET"])
-def demand():
-    return render_template("demand.html")
-
-
-@api.route("/demand/fft/<start_date>/<end_date>", methods=["GET", "POST"])
+# Define route for the FFT demand calculation and plots generation
+@api.route("/demand/fft/<start_date>/<end_date>", methods=["GET"])
 @cross_origin()
 def fft_demand(start_date, end_date):
     if request.method == "GET":
         start_date = convert_to_utc(start_date)
         end_date = convert_to_utc(end_date)
 
+        # Create a DemandRequest object and make a request to the esios API
         request_handler = DemandRequest(start_date, end_date)
         response = request_handler.make_request()
         signal = [item['value'] for item in response['indicator']['values']]
 
+        # Calculate FFT
         fft_calculator = FFTCalculator(signal)
         fft_result = fft_calculator.fft()
-        json_fft = fft_calculator.format_fft_to_json(fft_result)
+
+        # Create a FFTPlot object and plot the FFT
         fft_plot = FFTPlot(signal, fft_result)
         freq_fig = fft_plot.plot_frequency_domain(fft_result)
         time_fig = fft_plot.plot_time_domain()
 
+        # Set filenames for saving the plots
         freq_plot_filename = 'static/images/fig1.png'
         time_plot_filename = 'static/images/fig2.png'
 
+        # Upload plots to Cloudinary
         fft_plot.save_plot(freq_fig, freq_plot_filename)
         fft_plot.save_plot(time_fig, time_plot_filename)
 
@@ -69,18 +68,23 @@ def fft_demand(start_date, end_date):
         return (response)
 
     else:
-        return render_template("404.html"), 404
+        return 'Method not allowed'
+
+# Define route for the FFT demand calculation (JSON response)
 
 
 @api.route("/demand/fft/<start_date>/<end_date>/json", methods=["GET"])
 def fft_demand_json(start_date, end_date):
-    start_date = convert_to_utc(start_date)
-    end_date = convert_to_utc(end_date)
-    request_handler = DemandRequest(start_date, end_date)
-    response = request_handler.make_request()
-    signal = [item['value'] for item in response['indicator']['values']]
+    if request.method == "GET":
+        start_date = convert_to_utc(start_date)  # Convert start_date to UTC
+        end_date = convert_to_utc(end_date)  # Convert end_date to UTC
+        request_handler = DemandRequest(start_date, end_date)
+        response = request_handler.make_request()
+        signal = [item['value'] for item in response['indicator']['values']]
 
-    fft_calculator = FFTCalculator(signal)
-    fft_result = fft_calculator.fft()
-    json_fft = fft_calculator.format_fft_to_json(fft_result)
-    return json_fft
+        fft_calculator = FFTCalculator(signal)
+        fft_result = fft_calculator.fft()
+        json_fft = fft_calculator.format_fft_to_json(fft_result)
+        return json_fft
+    else:
+        return 'Method not allowed'
